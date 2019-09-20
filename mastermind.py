17b18ddc82af65f537cebb7aadacd60e29020084
WalkerTxsRngr7 <43824854+WalkerTxsRngr7@users.noreply.github.com>
2019-09-20 14:00:23 -0500

    colors and array for the rows created

--- mastermind.py
+++ mastermind.py
@@ -6,13 +6,63 @@ win=pygame.display.set_mode((700,700))
 pygame.display.set_caption("Mastermind")
 
 
-x = 50
+x = 100
 y = 50
 radius = 20
 width = 0
-vel = 60
+vel = 100
+rowDrawn = False
+row = 1
+
+w, h = 6, 12
+Rows = [[0 for x in range(w)] for y in range(h)] 
 
 run = True
+
+def drawRow():
+    global rowDrawn
+    global x
+    global y
+    global radius
+    global width
+    global vel
+    for i in range(4):
+        
+        
+
+        colorNum = random.randrange(6)
+
+        if (colorNum == 0):  #red
+            color = (255,0,0)
+        elif (colorNum == 1):    #orange
+            color = (255,128,0)
+        elif (colorNum == 2):    #yellow
+            color = (255,255,0)
+        elif (colorNum == 3):    #green
+            color = (0,255,0)
+        elif (colorNum == 4):    #blue
+            color = (0,0,255)
+        elif (colorNum == 5):    #pink
+            color = (255,0,255)
+
+        Rows[0][i] = colorNum
+        
+
+        pygame.draw.circle(win,color, (x,y), radius)
+
+        pygame.display.update()
+        x += 100
+        rowDrawn = True
+        i += 1
+    print (Rows[0])
+
+    
+
+#[[c1, c2, c3, c4, right, wrongSpot],[]]
+
+
+
+
 
 while run:
     pygame.time.delay(100)
@@ -31,7 +81,9 @@ while run:
         y -= vel
     if keys[pygame.K_DOWN]:
         y += vel
-    
+
+    if (rowDrawn == False):
+        drawRow()
 
     win.fill((0,0,0))
     i = 0
@@ -42,9 +94,3 @@ while run:
 pygame.quit()
 
 
-def drawRow():
-    for i in range(4):
-        i += 1
-        pygame.draw.circle(win,(255,0,0), (x,y), radius)
-        pygame.display.update()
-        x += 60
